1fbcb222274967c7a8a665e2cad60cc871dff50f
KostyaD <thedamaxstudio@gmail.com>
2014-07-22 20:29:57 +0400

    gallery

--- static/js/main.js
+++ static/js/main.js
@@ -171,3 +171,59 @@ var grphm_slider = (function(){
 
 $('.slider-container').slider(true);
 $('.auto-slider').slider();
+
+var Popup = (function(){
+
+	allow = true;
+	opened = false;
+	var show = function(popup) {
+		if(!allow) return;
+		$('.overlay').addClass('active').css('z-index', 99);
+		$('.pop-window[data-popup=' + popup + ']').removeClass('closed');
+		$('html').css('overflow', 'hidden');
+		opened = popup;
+	}
+
+	var close = function(popup) {
+		allow = false;
+		$('.overlay').removeClass('active');
+		$('html').removeAttr('style');
+		setTimeout(function(){
+			$('.overlay').css('z-index', -1);
+			$('.pop-window[data-popup=' + popup + ']').addClass('closed');
+			allow = true;
+			opened = false;
+		}, 500);
+	}
+
+	$(document).on('click', '.js-pop-close', function(){
+		if(opened) {
+			close(opened);
+		}
+		return false;
+	});
+
+	$(document).on('click', '.js-pop-show', function(){
+		if(!opened) {
+			show($(this).attr('data-popup'));
+		}
+		return false;
+	});
+
+	$(document).on('click', '.overlay', function(){
+		if(opened) {
+			close(opened);
+		}
+	});
+
+	$(document).on('click', '.pop-window', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+	});
+
+	if(window.location.hash != '') {
+		show(window.location.hash.substr(1));
+	}
+
+	return { show: show, close: close };
+})();
