a2f85b741b3f072b2e4e94e3ac6fa1a8b012542c
vkharseev <vkharseev@gmail.com>
2014-07-31 15:09:10 +0400

    Аксессуары

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -5,10 +5,10 @@ class DatabaseSeeder extends Seeder {
 	public function run(){
 		Eloquent::unguard();
 		
-//		$this->call('UserTableSeeder');
-//		$this->call('GroupsTableSeeder');
-//		$this->call('TablesSeeder');
-//		$this->call('ModulesTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('GroupsTableSeeder');
+		$this->call('TablesSeeder');
+		$this->call('ModulesTableSeeder');
 		$this->call('ProductAccessoriesTableSeeder');
 	}
 
