f0e6eaa9febc2186b6cf940240adfba80598d858
KostyaD <thedamaxstudio@gmail.com>
2014-08-02 19:14:43 +0400

    gallery fix

--- app/modules/production/views/galleries.blade.php
+++ app/modules/production/views/galleries.blade.php
@@ -6,24 +6,26 @@
 @section('content')
 @include('production/views/accepts/product-menu')
 @if($product->galleries->count())
-@foreach($product->galleries as $slider)
-    @if($slider->photos->count())
-<section class="gallery-block gallery-top">
-    <a href="#" class="gal-prev"></a>
-    <a href="#" class="gal-next"></a>
-    <div class="wrapper">
-        <div class="gallery-info">
-            <div class="title">{{ $product->meta->first()->title }}</div>
-            <div class="inf-text">{{ $product->meta->first()->preview }}</div>
+<div class="gallery">
+    @foreach($product->galleries as $slider)
+        @if($slider->photos->count())
+    <section class="gallery-block gallery-top">
+        <a href="#" class="gal-prev"></a>
+        <a href="#" class="gal-next"></a>
+        <div class="wrapper">
+            <div class="gallery-info">
+                <div class="title">{{ $product->meta->first()->title }}</div>
+                <div class="inf-text">{{ $product->meta->first()->preview }}</div>
+            </div>
         </div>
-    </div>
-    <div class="gallery-photo">
-        @foreach($slider->photos as $photo)
-        <div class="gallery-img" style="background-image: url({{ asset('uploads/galleries/'.$photo->name) }})"></div>
-        @endforeach
-    </div>
-</section>
-    @endif
+        <div class="gallery-photo">
+            @foreach($slider->photos as $photo)
+            <div class="gallery-img" style="background-image: url({{ asset('uploads/galleries/'.$photo->name) }})"></div>
+            @endforeach
+        </div>
+    </section>
+        @endif
+</div>
 @endforeach
 @endif
 @if($product->videos->count())
