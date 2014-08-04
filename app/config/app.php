5fdbff71f5d529b3d14dab8bb6fb637d268fe456
sngrl <reserved@mail.ru>
2014-08-04 17:51:55 +0400

    '+'

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	'template' => 'site',
 	'manifest' => storage_path('meta'),
 
-	'debug' => true,
+	'debug' => FALSE,
 	'url' => 'http://www.infiniti-gedon.ru/',
 	'timezone' => 'UTC',
 	'locale' => 'ru',
