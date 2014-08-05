2139824bb5bb2b8137eebd20c61c3f67266bc48a
sngrl <reserved@mail.ru>
2014-08-05 16:37:17 +0400

    '+'

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         },
         files: {
           // target.css file: source.less file
-          "css/main.css": "less/main.less"
+          "../public/theme/css/main.css": "less/main.less"
         }
       }
     },
