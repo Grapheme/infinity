6ebb1792fd8427219b978615b81fa85ed33f1da0
sngrl <reserved@mail.ru>
2014-08-08 12:46:01 +0400

    +

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -4,11 +4,16 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="{{asset('js/vendor/jquery.min.js');}}"><\/script>')</script>
 @endif
+
+    {{-- Вова, охуеть --}}
+    if (0)
 	{{HTML::script('js/system/main.js');}}
 	{{HTML::script('js/vendor/SmartNotification.min.js');}}
 	{{HTML::script('js/vendor/jquery.validate.min.js');}}
 	{{HTML::script('js/system/app.js');}}
 	{{HTML::script('js/system/messages.js');}}
+    @endif
+
 	{{HTML::script('theme/js/plugins.js');}}
 	<script type="text/javascript">
 		if(typeof runFormValidation === 'function'){
