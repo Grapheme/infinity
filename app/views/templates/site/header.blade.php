fdce59e0ae34ab572a9c5ddb118ea08b30994dc3
KostyaD <thedamaxstudio@gmail.com>
2014-08-11 19:13:22 +0400

    car desc

--- app/views/templates/site/header.blade.php
+++ app/views/templates/site/header.blade.php
@@ -262,7 +262,7 @@
             <div class="main-block">
             @endif
                 <div class="car-name">{{ $product->meta->first()->short_title }}</div>
-                {{ $product->meta->first()->in_menu_content }}
+                <div claas="car-desc">{{ $product->meta->first()->in_menu_content }}</div>
                 <div class="car-btns">
                     <a href="{{ link::to(ProductionController::$prefix_url.'/'.$product->meta->first()->seo_url) }}" class="drive-btn"><span class="icon icon-page"></span>Подробнее</a>
                     <!--<a href="javascript:void(0);" class="drive-btn js-pop-show" data-popup="test-drive" data-model="{{ $product->meta->first()->title }}"><span class="icon icon-wheel"></span>Записаться на тестдрайв</a>
@@ -278,7 +278,7 @@
             <div class="main-block">
             @endif
                 <div class="car-name">{{ $related_product->meta->first()->title }}</div>
-            {{ $related_product->meta->first()->in_menu_content }}
+                <div class="car-desc">{{ $related_product->meta->first()->in_menu_content }}</div>
                 <div class="car-btns">
                     <a href="{{ link::to(ProductionController::$prefix_url.'/'.$related_product->meta->first()->seo_url) }}" class="drive-btn"><span class="icon icon-page"></span>Подробнее</a>
                     <!--<a href="javascript:void(0);" class="drive-btn js-pop-show" data-popup="test-drive" data-model="{{ $product->meta->first()->title }}"><span class="icon icon-wheel"></span>Записаться на тестдрайв</a>
