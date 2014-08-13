b9188102875aec1795bb317e6a005d084d0ae51f
KostyaD <thedamaxstudio@gmail.com>
2014-08-13 15:03:49 +0400

    404 страницв

--- app/views/error404.blade.php
+++ app/views/error404.blade.php
@@ -3,35 +3,32 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<head>
-    @include('templates.default.head')
+ <head>
+    @include('templates.site.head')
 </head>
 <body>
-<!--[if lt IE 7]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
-    your browser</a> to improve your experience.</p>
-<![endif]-->
-<div class="wrapper-full{{ Request::is('/') ? ' main-page' : '' }}">
-    @include('templates.default.header')
+    <!--[if lt IE 7]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+        your browser</a> to improve your experience.</p>
+    <![endif]-->
+    @include('templates.site.header')
     <main>
-        <div class="wrapper">
-            <div class="not-found">
-                <div class="nf-block">
-                    <div class="us-title">404 ошибка</div>
-                    <div class="us-text">
-                        Запрашиваемая вами страница
-                        не найдена. Вернитесь на <a href="/">главную</a>
-                        страницу сайта.
+        <div class="not-found">
+            <div class="nf-block">
+                <div class="nf-left">404</div>
+                <div class="nf-right">
+                    <div class="title">Ошибка</div>
+                    <div class="desc">
+                        Запрашиваемая вами страница<br>
+                        не найдена. Вернитесь на <a href="{{URL::to('/')}}">главную</a><br>
+                        страницу сайта
                     </div>
                 </div>
             </div>
         </div>
     </main>
-    <div class="wrapper-nf">
-        <div class="nf-img"></div>
-    </div>
-</div>
-@include('templates.default.scripts')
-@yield('scripts')
+    @include('templates.site.scripts')
+    {{ HTML::script('theme/js/vendor/jquery.sumoselect.min.js') }}
+    {{ HTML::script('theme/js/main.js') }}
 </body>
 </html>
