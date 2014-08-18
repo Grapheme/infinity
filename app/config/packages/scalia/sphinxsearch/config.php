f63db1e259e265afad72989fea7450a0ebd310ab
Владимир <vkharseev@gmail.com>
2014-08-18 19:51:24 +0400

    Поиск

--- app/config/packages/scalia/sphinxsearch/config.php
+++ app/config/packages/scalia/sphinxsearch/config.php
@@ -3,7 +3,11 @@
 return array (
 	'host'    => '127.0.0.1',
 	'port'    => 9312,
-	'indexes' => array (
-		'my_index_name' => array ( 'table' => 'keywords', 'column' => 'id' ),
-	)
+    'indexes' => array (
+        'channelsIndexInfinity' => array('table'=>'channels','column'=>'id'),
+        'productsIndexInfinity' => array('table'=>'products','column'=>'id'),
+        'productsAccessibilityIndexInfinity' => array('table'=>'products_accessories','column'=>'id'),
+        'newsIndexInfinity' => array('table'=>'i18n_news','column'=>'id'),
+        'pagesIndexInfinity' => array('table'=>'i18n_pages','column'=>'id')
+    )
 );
