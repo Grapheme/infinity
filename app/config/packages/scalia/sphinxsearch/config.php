ba548d4de38551909170d307ad200c95de984d84
Владимир <vkharseev@gmail.com>
2014-08-19 12:04:53 +0400

    Поиск

--- app/config/packages/scalia/sphinxsearch/config.php
+++ app/config/packages/scalia/sphinxsearch/config.php
@@ -1,13 +1,13 @@
 <?php
 
 return array (
-	'host'    => '127.0.0.1',
-	'port'    => 3306,
+    'host'    => '127.0.0.1',
+    'port'    => 3312,
     'indexes' => array (
-        'channelsIndexInfinity' => array('table'=>'channels','column'=>'id'),
-        'productsIndexInfinity' => array('table'=>'products','column'=>'id'),
-        'productsAccessibilityIndexInfinity' => array('table'=>'products_accessories','column'=>'id'),
-        'newsIndexInfinity' => array('table'=>'i18n_news','column'=>'id'),
-        'pagesIndexInfinity' => array('table'=>'i18n_pages','column'=>'id')
+        'channelsIndexInfinity' => array('table'=>'channels','column'=>'id','modelname'=>'Channel'),
+        'productsIndexInfinity' => array('table'=>'products','column'=>'id','modelname'=>'Product'),
+        'productsAccessibilityIndexInfinity' => array('table'=>'products_accessories','column'=>'id','modelname'=>'ProductAccessory'),
+        'newsIndexInfinity' => array('table'=>'i18n_news','column'=>'id','modelname'=>'I18nNews'),
+        'pagesIndexInfinity' => array('table'=>'i18n_pages','column'=>'id','modelname'=>'I18nPage')
     )
 );
