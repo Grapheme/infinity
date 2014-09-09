0b015dea560169824319747d1698c79feb2b3b9f
Владимир <vkharseev@gmail.com>
2014-09-09 18:45:06 +0400

    +

--- app/config/vkharseev/database.php
+++ app/config/vkharseev/database.php
@@ -6,7 +6,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'infinity',
+			'database'  => 'infiniti',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
