3fe9ba80ea271cc764893b713ba7e3a88dc42e4d
Владимир <vkharseev@gmail.com>
2014-11-13 14:42:55 +0300

    public_html

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -3,7 +3,7 @@
 return array(
 
 	'app' => __DIR__.'/../app',
-	'public' => __DIR__.'/../public/',
+	'public' => __DIR__.'/../public_html/',
 	'base' => __DIR__.'/..',
 	'storage' => __DIR__.'/../app/storage',
 );
