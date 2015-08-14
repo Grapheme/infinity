e24f4b87dca02267b5a7557c533e4c862fc46b4b
Владимир <vkharseev@gmail.com>
2015-08-14 12:55:27 +0300

    Сделать вывод ссылки со слайдера на главной

--- app/modules/channels/admin_channel.controller.php
+++ app/modules/channels/admin_channel.controller.php
@@ -126,7 +126,7 @@ class AdminChannelController extends BaseController {
         $json_request = array('status'=>FALSE, 'responseText'=>'', 'responseErrorText'=>'', 'redirect'=>FALSE, 'gallery'=>0);
         $validation = Validator::make(Input::all(), Product::$rules);
         if($validation->passes()):
-            $channel = $this->channel->find($id);
+            $channel = $this->channel->where('id', $id)->first();
             self::saveChannelModel($channel);
             $json_request['gallery'] = self::saveChannelGallery();
             $json_request['responseText'] = 'Элемент канала обновлен';
@@ -147,7 +147,7 @@ class AdminChannelController extends BaseController {
         Allow::permission($this->module['group'], 'channel_delete');
         $json_request = array('status'=>FALSE, 'responseText'=>'');
         if(Request::ajax()):
-            $channel = $this->channel->find($id);
+            $channel = $this->channel->where('id', $id)->first();
             if (!empty($channel->file) && File::exists(public_path($channel->file))):
                 File::delete(public_path($channel->file));
             endif;
